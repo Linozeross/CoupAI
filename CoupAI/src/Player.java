4e0e7806012552664d6857cce84c7cce77b59973
chameleonchild <gadachikriege@gmail.com>
2018-02-18 01:38:59 -0500

    Add files via upload

--- CoupAI/src/Player.java
+++ CoupAI/src/Player.java
@@ -3,8 +3,10 @@ public class Player {
 	int money;
 	String[] cards = new String[2];
 	
-	public Player(int money) {
-		this.money = money;
+	public Player(int money, String card1, String card2) {
+		money = money;
+		cards[0] = card1;
+		cards[1] = card2;
 	}
 
 	public int getMoney() {
@@ -19,14 +21,42 @@ public class Player {
 		return cards;
 	}
 
-	public void setCard1(String card1) {
-		this.cards[0] = card1;
+	public void setCards(String[] cards) {
+		this.cards = cards;
 	}
 	
-	public void setCard2(String card2) {
-		this.cards[1] = card2;
+	public void income() {
+		this.money++;
+	}
+	
+	public void foreignAid(){
+		this.money += 2;
+	}
+	
+	public void coup(Player player) {
+		//TODO preconditions?
+		//
+		this.money -= 7;
+		// target player must choose a card to lose
+	}
+	
+	public void tax() {
+		this.money += 3;
+	}
+	
+	public void assassinate(Player player) {
+		this.money -= 3;
+		//target player must choose a card to lose
+	}
+	
+	public void exchange() {
+		assert false;
+	}
+	
+	public void steal(Player other) {
+		this.money += 2;
+		other.money -= 2;
 	}
 	
 	
-
 }
