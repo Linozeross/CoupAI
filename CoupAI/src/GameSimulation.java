705a6df12f30d5c48bf02553a8d33868a4e9f2cb
Linozeross <lino@toranjenner.de>
2018-02-18 01:53:59 -0500

    Implemented Tax

--- CoupAI/src/GameSimulation.java
+++ CoupAI/src/GameSimulation.java
@@ -1,20 +1,16 @@
-import java.util.ArrayList;
-import java.util.Collections;
+
 import java.util.Scanner;
-import java.util.Stack;
-import java.util.concurrent.ThreadLocalRandom;
+
 
 
 public class GameSimulation {
 	
-	
-	
+
 	public static void main(String[] args) {
 		
 		GameState state = new GameState();
 		Scanner sc = new Scanner(System.in);
 		
-				
 		//give players cards
 		//implemented randomly for now TODO: make it according to two player rules.
 				
@@ -25,12 +21,29 @@ public class GameSimulation {
 		
 		
 		//currentPlayer  turn
-		System.out.println("What is your action)");
+		System.out.println("What is your action?");
 		
 		String action = sc.nextLine();
 		
+		
+		if (action.equals("tax")) {
+			System.out.println("Do you want to challenge this action?");
+			if (sc.nextLine().equals("yes")) {
+				state.challenge("tax");
+			}
+			
+			
+			state.foreignaid()
+		}
+		//check input
+		
+		state.assinate(p1,p2);
+		
 		//evaluate action
+		
+		
 		//if action is challengable
+		
 		System.out.println("Do you want to challenge this action?");
 		String challenge = sc.nextLine();
 		//if yes:
--- CoupAI/src/GameState.java
+++ CoupAI/src/GameState.java
@@ -2,82 +2,95 @@ import java.util.Collections;
 import java.util.Stack;
 
 public class GameState {
-	Player p1; 
-	Player p2; 
+	Player p1;
+	Player p2;
 	Stack<String> court;
 	Player currentPlayer;
-	
-	public GameState(){
+	Player otherPlayer;
+
+	public GameState() {
 		p1 = new Player(1);
 		p2 = new Player(2);
-		court = startingCards(p1,p2);
+		court = startingCards(p1, p2);
 		currentPlayer = p1;
+		otherPlayer = p2;
 	}
-	
-	
+
 	private Stack<String> startingCards(Player p1, Player p2) {
-		/** creates the court Stack and gives the starting cards out to players.
+		/**
+		 * creates the court Stack and gives the starting cards out to players.
 		 * 
 		 **/
-		
+
 		// TODO Auto-generated method stub
-			Stack<String> court = new Stack<String>();
-			for (int i=0; i<=2; i++) {
-				court.push("Duke");
-				court.push("Assasin");
-				court.push("Ambassador");
-				court.push("Captain");
-				court.push("Contessa");
-			}
+		Stack<String> court = new Stack<String>();
+		for (int i = 0; i <= 2; i++) {
+			court.push("Duke");
+			court.push("Assasin");
+			court.push("Ambassador");
+			court.push("Captain");
+			court.push("Contessa");
+		}
 		Collections.shuffle(court);
-		
+
 		p1.setCard1(court.pop());
 		p1.setCard2(court.pop());
 		p2.setCard1(court.pop());
 		p2.setCard2(court.pop());
-	
+
 		return court;
 	}
 
-
+	public void challenge(String action) {
+		if (action.equals("tax")) {
+		 
+			//check if current player has Duke
+			if (currentPlayer.getCards()[0].equals("Duke") || currentPlayer.getCards()[1].equals("Duke")) {
+				otherPlayer.die()
+			}
+			else {
+				currentPlayer.die()
+			}
+			
+			
+		}
+		
+	}
+	
+	
+	
+	
+	
 	public Player getP1() {
 		return p1;
 	}
-
 
 	public void setP1(Player p1) {
 		this.p1 = p1;
 	}
 
-
 	public Player getP2() {
 		return p2;
 	}
-
 
 	public void setP2(Player p2) {
 		this.p2 = p2;
 	}
 
-
 	public Stack<String> getCourt() {
 		return court;
 	}
-
 
 	public void setCourt(Stack<String> court) {
 		this.court = court;
 	}
 
-
 	public Player getCurrentPlayer() {
 		return currentPlayer;
 	}
 
-
 	public void setCurrentPlayer(Player currentPlayer) {
 		this.currentPlayer = currentPlayer;
 	}
-	
 
 }
